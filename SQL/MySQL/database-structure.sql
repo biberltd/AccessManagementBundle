a41884d36a1b82acc7b5b14c53487dab2b0a5ec2
Özge Deniz Uysal <ozgedenizuysal@MacBook-Ozg.local>
2016-08-16 17:30:15 +0300

    Index isimleri düzenlendi.

--- SQL/MySQL/database-structure.sql
+++ SQL/MySQL/database-structure.sql
@@ -12,15 +12,15 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for member_access_right
 -- ----------------------------
 DROP TABLE IF EXISTS `member_access_right`;
-  CREATE TABLE `member_access_right` (
+CREATE TABLE `member_access_right` (
   `member` int(10) unsigned NOT NULL COMMENT 'Member who takes the action.',
   `action` int(5) unsigned NOT NULL COMMENT 'Action that member logged.',
   `right` varchar(1) COLLATE utf8_turkish_ci NOT NULL DEFAULT 'g' COMMENT 'Access right.',
   `date_assigned` datetime NOT NULL COMMENT 'Date when the right is assigned.',
   PRIMARY KEY (`member`,`action`),
-  UNIQUE KEY `idxUMemberAction` (`member`,`action`) USING BTREE,
-  KEY `idxNDateAssigned` (`date_assigned`) USING BTREE,
-  KEY `idxNAction` (`action`) USING BTREE,
+  UNIQUE KEY `idxUMemberAccessRightMemberAction` (`member`,`action`) USING BTREE,
+  KEY `idxNMemberAccessRightDateAssigned` (`date_assigned`) USING BTREE,
+  KEY `idxNMemberAccessRightAction` (`action`) USING BTREE,
   CONSTRAINT `idxFMemberOfMemberAccessRight` FOREIGN KEY (`member`) REFERENCES `member` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `idxFActionOfMemberAccessRight` FOREIGN KEY (`action`) REFERENCES `action` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci ROW_FORMAT=COMPACT;
@@ -29,15 +29,15 @@ DROP TABLE IF EXISTS `member_access_right`;
 -- Table structure for member_group_access_right
 -- ----------------------------
 DROP TABLE IF EXISTS `member_group_access_right`;
-  CREATE TABLE `member_group_access_right` (
+CREATE TABLE `member_group_access_right` (
   `member_group` int(10) unsigned NOT NULL COMMENT 'Member group.',
   `action` int(5) unsigned NOT NULL COMMENT 'Action.',
   `right` varchar(1) COLLATE utf8_turkish_ci NOT NULL DEFAULT 'g' COMMENT 'g:granted,r:revoked',
   `date_assigned` datetime NOT NULL COMMENT 'Date when the right is assigned.',
   PRIMARY KEY (`member_group`,`action`),
-  UNIQUE KEY `idxUMemberGroupAction` (`member_group`,`action`) USING BTREE,
-  KEY `idxNAction` (`action`) USING BTREE,
-  KEY `idxNDateAssigned` (`date_assigned`) USING BTREE,
+  UNIQUE KEY `idxUMemberGroupAccessRightMemberGroupAction` (`member_group`,`action`) USING BTREE,
+  KEY `idxNMemberGroupAccessRightAction` (`action`) USING BTREE,
+  KEY `idxNMemberGroupAccessRightDateAssigned` (`date_assigned`) USING BTREE,
   CONSTRAINT `idxFActionOfMemberGroupAccessRight` FOREIGN KEY (`action`) REFERENCES `action` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `idxFMemberGroupOfMemberGroupAccessRight` FOREIGN KEY (`member_group`) REFERENCES `member_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci ROW_FORMAT=COMPACT;
